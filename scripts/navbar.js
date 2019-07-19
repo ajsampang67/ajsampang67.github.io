776bb9a76f60f942dbfca884f92d046529cd81ea
ajsampang67 <ajsampang67@gmail.com>
2019-07-19 12:01:52 -0500

    Cleaned up navbar, updated about me page with family, added placeholders for new pages

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
     $(window).on("scroll", function() {
-        if ($(window).scrollTop() >= 200) {
+        if ($(window).scrollTop() >= 250) {
             $(".navbar").addClass("compressed");
-            document.getElementById("me").style.maxWidth="5%";
+            document.getElementById("me").style.maxWidth="4%";
             $('#navbar-header-text').hide();
+            $('#navbarNav').show();
 
         } else {
             $(".navbar").removeClass("compressed");
             document.getElementById("me").style.maxWidth="30%";
             $('#navbar-header-text').show();
+            $('#navbarNav').hide();
         }
     });
 });
