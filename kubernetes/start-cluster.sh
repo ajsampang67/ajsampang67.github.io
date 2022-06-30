10f3b2c3ddb041de80b8df5d85a56238b81084ac
Alfred Sampang <ajsampang67@gmail.com>
2022-06-29 23:51:02 -0500

    Update start-cluster.sh

--- kubernetes/start-cluster.sh
+++ kubernetes/start-cluster.sh
@@ -8,5 +8,5 @@ fi
 
 helm upgrade --install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace
 helm upgrade --install database bitnami/mysql -f database/values-$1.yaml 
-helm upgrade --install ajtheamazing dxp/ -f ajtheamazing/values-$1.yaml
-helm upgrade --install blog dxp/ -f blog/values-$1.yaml
+helm upgrade --install ajtheamazing ajtheamazing/ -f ajtheamazing/values-$1.yaml
+helm upgrade --install blog blog/ -f blog/values-$1.yaml
